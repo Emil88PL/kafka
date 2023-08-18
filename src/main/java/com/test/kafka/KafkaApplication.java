cefd6e3d8d855d0fd83fc8d306cb157b61b3a564
Emil <emil88pl@gmail.com>
2023-08-18 17:03:10 +0100

    create conf topic

--- src/main/java/com/test/kafka/KafkaApplication.java
+++ src/main/java/com/test/kafka/KafkaApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KafkaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+    
 }
